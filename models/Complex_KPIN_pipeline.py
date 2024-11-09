eb837110e8fa6c2b2a5829e9761d06e5afce89f5
sunyiyong <yiyongsun@link.cuhk.edu.cn>
2024-11-09 20:44:50 +0800

    Add files via upload

---
+++ models/Complex_KPIN_pipeline.py
@@ -0,0 +1,299 @@
+"""
+This file defines the module we used:
+
+Gnet_pipeline module: to train and test KPIN.
+
+Author: SUN, Yiyong
+Date: 2024.6.30
+"""
+
+import torch
+import torch.nn as nn
+import random
+import time
+from time import strftime, gmtime
+
+
+
+class Gnet_pipeline:
+
+    def __init__(self, global_vars ):
+        super().__init__()
+
+        self.dev                     = global_vars.init_dev
+        self.p                       = global_vars.p
+        self.m                       = global_vars.m
+        self.n                       = global_vars.n
+        self.pm                      = global_vars.pm
+        self.Q_re                    = global_vars.Q.real
+        self.Q_im                    = global_vars.Q.imag
+        #
+        self.num_all_sub             = global_vars.KPIN_train_num_all_sub
+        self.pretrain_len_sub        = global_vars.KPIN_train_len_sub
+        self.num_pretest             = global_vars.KPIN_num_test
+        self.Gnet_pretest_start      = global_vars.KPIN_test_start
+        self.pretest_len             = global_vars.KPIN_test_len
+        self.Gnet_pretrain_len       = global_vars.KPIN_train_len
+        #
+        self.pretrain_num_epoch      = global_vars.KPIN_train_num_epoch
+        self.pretrain_batchsize      = global_vars.KPIN_train_batchsize
+        #
+        self.pretrain_learningRate   = global_vars.KPIN_train_learningRate
+        self.pretrain_weightDecay    = global_vars.KPIN_train_weightDecay
+        #
+        self.Gnet                    = global_vars.Gnet
+        self.optimizer               = torch.optim.Adam(self.Gnet.parameters(), lr=self.pretrain_learningRate, weight_decay=self.pretrain_weightDecay)
+
+    def KPIN_train(self, global_vars):
+        self.time_Gnet_pretrain = 0.0
+        start_pretrain = time.time()
+        pretrain_input = global_vars.Gnet_pretrain_input
+        pretrain_target = global_vars.Gnet_pretrain_target
+        self.num_epochs = global_vars.KPIN_train_num_epoch
+        epsilon = global_vars.KPIN_epsilon
+
+        self.nmse_hLoss_prior_epoch = torch.zeros([self.num_epochs]).to(self.dev)
+        self.nmse_hLoss_prior_epoch_dB = torch.zeros([self.num_epochs]).to(self.dev)
+
+        self.nmse_yLoss_epoch = torch.zeros([self.num_epochs]).to(self.dev)
+        self.nmse_yLoss_epoch_dB = torch.zeros([self.num_epochs]).to(self.dev)
+
+        self.nmse_hLoss_posterior_epoch = torch.zeros([self.num_epochs]).to(self.dev)
+        self.nmse_hLoss_posterior_epoch_dB = torch.zeros([self.num_epochs]).to(self.dev)
+
+        self.nmse_pretest_epoch = torch.zeros([self.num_epochs]).to(self.dev)
+        self.nmse_pretest_epoch_dB = torch.zeros([self.num_epochs]).to(self.dev)
+
+        self.Gnet_prior = torch.zeros(self.pretest_len, self.p * self.m,
+                                      dtype=torch.complex64).to(self.dev)
+        self.Gnet_posterior = torch.zeros(self.pretest_len, self.p * self.m,
+                                          dtype=torch.complex64).to(self.dev)
+
+        ti = 0
+        # Training
+        while True and ti < self.num_epochs:
+            self.ti = ti
+            self.Gnet.train()
+
+            LOSS_h_prior = torch.tensor([0.]).to(self.dev)
+            LOSS_y_prior = torch.tensor([0.]).to(self.dev)
+            self.h_prior = torch.zeros(self.pretrain_batchsize, self.m,
+                                       self.pretrain_len_sub, dtype=torch.complex64).to(
+                self.dev)
+            self.x_prior = torch.zeros(self.pretrain_batchsize, self.pm,
+                                       self.pretrain_len_sub, dtype=torch.complex64).to(
+                self.dev)
+            self.y_prior = torch.zeros(self.pretrain_batchsize, self.n,
+                                       self.pretrain_len_sub, dtype=torch.complex64).to(
+                self.dev)
+            self.nmse_hLoss_in_batch = torch.zeros(self.pretrain_batchsize,
+                                                   self.pretrain_len_sub).to(self.dev)
+            self.nmse_yLoss_in_batch = torch.zeros(self.pretrain_batchsize,
+                                                   self.pretrain_len_sub).to(self.dev)
+
+            LOSS_h_posterior = torch.tensor([0.]).to(self.dev)
+            self.h_posterior = torch.zeros(self.pretrain_batchsize, self.m,
+                                           self.pretrain_len_sub,
+                                           dtype=torch.complex64).to(self.dev)
+            self.nmse_hLoss_posterior_in_batch = torch.zeros(self.pretrain_batchsize,
+                                                             self.pretrain_len_sub).to(
+                self.dev)
+
+            start_epoch = time.time()
+            for j in range(0, self.pretrain_batchsize):
+                if global_vars.KPIN_train_initSSM_by_KF_posterior:
+                    n_e = random.randint(1, self.num_all_sub - 1)
+                    last_step = n_e * self.pretrain_len_sub - 1
+                    self.Gnet.m1x_prior = global_vars.KFilter.KFilter_prior[last_step,
+                                          :].to(self.dev)
+                    self.Gnet.m1x_posterior = global_vars.KFilter.KFilter_posterior[
+                                              last_step, :].to(self.dev)
+                else:
+                    n_e = random.randint(0, self.num_all_sub - 1)
+                    self.Gnet.m1x_prior = torch.zeros(self.pm, dtype=torch.complex64).to(
+                        self.dev)
+                    self.Gnet.m1x_posterior = torch.zeros(self.pm,
+                                                          dtype=torch.complex64).to(
+                        self.dev)
+
+                self.Gnet.hn = torch.zeros(1, 1, global_vars.KPIN_GRU_hn_dim).to(self.dev)
+                for t in range(0, self.pretrain_len_sub):
+                    self.x_prior[j, :, t], self.y_prior[j, :, t] = self.Gnet.one_ssm_loop(
+                        pretrain_input[n_e, :, t], global_vars)
+                    self.h_prior[j, :, t] = self.x_prior[j, :, t][:self.m]
+                    self.h_posterior[j, :, t] = self.Gnet.m1x_posterior[:self.m]
+                    torch.cuda.empty_cache()
+
+                    diff_h_abs = torch.abs(
+                        self.h_prior[j, :, t] - pretrain_target[n_e, :, t])
+                    h_abs = torch.abs(pretrain_target[n_e, :, t])
+                    self.nmse_hLoss_in_batch[j, t] = torch.norm(
+                        diff_h_abs) ** 2 / torch.norm(h_abs) ** 2
+
+                    diff_y_abs = torch.abs(
+                        self.y_prior[j, :, t] - pretrain_input[n_e, :, t])
+                    y_abs = torch.abs(pretrain_input[n_e, :, t])
+                    self.nmse_yLoss_in_batch[j, t] = torch.norm(
+                        diff_y_abs) ** 2 / torch.norm(y_abs) ** 2
+
+                    diff_h_posterior_abs = torch.abs(
+                        self.h_posterior[j, :, t] - pretrain_target[n_e, :, t])
+                    h_abs = torch.abs(pretrain_target[n_e, :, t])
+                    self.nmse_hLoss_posterior_in_batch[j, t] = torch.norm(
+                        diff_h_posterior_abs) ** 2 / torch.norm(h_abs) ** 2
+
+                LOSS_h_prior = LOSS_h_prior + torch.mean(self.nmse_hLoss_in_batch, dim=1)[j]
+                LOSS_y_prior = LOSS_y_prior + torch.mean(self.nmse_yLoss_in_batch, dim=1)[j]
+                LOSS_h_posterior = LOSS_h_posterior + torch.mean(self.nmse_hLoss_posterior_in_batch, dim=1)[j]
+
+            torch.cuda.empty_cache()
+            self.nmse_hLoss_in_batch = torch.mean(self.nmse_hLoss_in_batch, dim=1)
+            self.nmse_yLoss_in_batch = torch.mean(self.nmse_yLoss_in_batch, dim=1)
+            self.nmse_hLoss_posterior_in_batch = torch.mean(self.nmse_hLoss_posterior_in_batch, dim=1)
+
+            self.nmse_hLoss_prior_epoch[ti] = torch.mean(self.nmse_hLoss_in_batch)
+            self.nmse_yLoss_epoch[ti] = torch.mean(self.nmse_yLoss_in_batch)
+            self.nmse_hLoss_prior_epoch_dB[ti] = 10 * torch.log10(self.nmse_hLoss_prior_epoch[ti])
+            self.nmse_yLoss_epoch_dB[ti] = 10 * torch.log10(self.nmse_yLoss_epoch[ti])
+
+            self.nmse_hLoss_posterior_epoch[ti] = torch.mean(
+                self.nmse_hLoss_posterior_in_batch)
+            self.nmse_hLoss_posterior_epoch_dB[ti] = 10 * torch.log10(
+                self.nmse_hLoss_posterior_epoch[ti])
+
+            if global_vars.KPIN_supervised_by == "h_prior":
+                LOSS_mean = LOSS_h_prior / self.pretrain_batchsize
+            elif global_vars.KPIN_supervised_by == "h_posterior":
+                LOSS_mean = LOSS_h_posterior / self.pretrain_batchsize
+            elif global_vars.KPIN_supervised_by == "y_predict_noisy":
+                LOSS_mean = LOSS_y_prior / self.pretrain_batchsize
+            else:
+                pass
+
+            self.optimizer.zero_grad()
+            LOSS_mean.backward()
+            torch.cuda.empty_cache()
+            self.optimizer.step()
+            torch.cuda.empty_cache()
+            self.optimizer.zero_grad()
+
+            end_epoch = time.time()
+            self.time_epoch = end_epoch - start_epoch
+            self.time_Gnet_pretrain = self.time_Gnet_pretrain + self.time_epoch
+
+            if global_vars.KPIN_supervised_by == "h_prior":
+                diff_hLoss = self.nmse_hLoss_prior_epoch_dB[ti] - \
+                             self.nmse_hLoss_prior_epoch_dB[ti - 1]
+                print(
+                    "KPIN Epoch: {},".format(ti),
+                    "hLoss: {:.2f}[dB],".format(
+                        self.nmse_hLoss_prior_epoch_dB[ti].item()),
+                    "diff_hLoss: {:.2f}[dB]".format(diff_hLoss),
+                    "time: {:.2f}[s]".format(self.time_epoch)
+                )
+            elif global_vars.KPIN_supervised_by == "h_posterior":
+                diff_hLoss_posterior = self.nmse_hLoss_posterior_epoch_dB[ti] - \
+                                       self.nmse_hLoss_posterior_epoch_dB[ti - 1]
+                print(
+                    "KPIN Epoch: {},".format(ti),
+                    "hLoss: {:.2f}[dB],".format(
+                        self.nmse_hLoss_posterior_epoch_dB[ti].item()),
+                    "diff_hLoss: {:.2f}[dB]".format(diff_hLoss_posterior),
+                    "time: {:.2f}[s]".format(self.time_epoch)
+                )
+            elif global_vars.KPIN_supervised_by == "y_predict_noisy":
+                diff_yLoss = self.nmse_yLoss_epoch_dB[ti] - self.nmse_yLoss_epoch_dB[
+                    ti - 1]
+                print(
+                    "KPIN Epoch: {},".format(ti),
+                    "yLoss: {:.2f}[dB],".format(self.nmse_yLoss_epoch_dB[ti].item()),
+                    "diff_yLoss: {:.2f}[dB]".format(diff_yLoss),
+                    "time: {:.2f}[s]".format(self.time_epoch)
+                )
+
+            if self.ti == self.num_epochs - 1:
+                self.KPIN_test(global_vars)
+                self.optimizer.zero_grad()
+                torch.cuda.empty_cache()
+
+            if self.ti == self.num_epochs - 1:
+                end_pretrain = time.time()
+                time_pretrain = end_pretrain - start_pretrain
+                print(
+                    "Total Train Time: " + strftime("%H:%M:%S", gmtime(time_pretrain))
+                )
+
+            if ti > 0 and LOSS_mean < epsilon:
+                self.KPIN_test(global_vars)
+                self.optimizer.zero_grad()
+                torch.cuda.empty_cache()
+                break
+
+            ti += 1
+            torch.cuda.empty_cache()
+
+        global_vars.Gnet_pretrain_num_epoch_actual = ti
+
+    def KPIN_test(self, global_vars):
+        test_input = global_vars.Gnet_pretest_input
+        test_target = global_vars.Gnet_pretest_target
+
+        self.Gnet.eval()
+        torch.no_grad()
+
+        self.out_test_h = torch.zeros(self.num_pretest, self.m, self.pretest_len,
+                                      dtype=torch.complex64).to(self.dev)
+        self.out_test_x = torch.zeros(self.num_pretest, self.pm, self.pretest_len,
+                                      dtype=torch.complex64).to(self.dev)
+        self.out_test_y = torch.zeros(self.num_pretest, self.n, self.pretest_len,
+                                      dtype=torch.complex64).to(self.dev)
+        self.nmse_Gnet_pretest = torch.zeros(self.num_pretest, self.pretest_len).to(
+            self.dev)
+        self.out_test_KG = torch.zeros(self.num_pretest, self.pretest_len, self.pm,
+                                       self.n, dtype=torch.complex64).to(self.dev)
+
+        start_test = time.time()
+        for j in range(0, self.num_pretest):
+            self.Gnet.m1x_prior = global_vars.KFilter.KFilter_prior[
+                                  self.Gnet_pretrain_len - 1, :].to(self.dev)
+            self.Gnet.m1x_posterior = global_vars.KFilter.KFilter_posterior[
+                                      self.Gnet_pretrain_len - 1, :].to(self.dev)
+            self.Gnet.hn = torch.zeros(1, 1, global_vars.KPIN_GRU_hn_dim).to(self.dev)
+
+            for t in range(0, self.pretest_len):
+                self.out_test_x[j, :, t], self.out_test_y[j, :,
+                                          t] = self.Gnet.one_ssm_loop(test_input[j, :, t],
+                                                                      global_vars)
+                self.out_test_h[j, :, t] = self.out_test_x[j, :, t][:self.m]
+
+                if self.ti == global_vars.KPIN_train_num_epoch - 1:
+                    self.Gnet_prior[t, :] = self.Gnet.m1x_prior
+                    self.Gnet_posterior[t, :] = self.Gnet.m1x_posterior
+
+                diff_h_abs = torch.abs(self.out_test_h[j, :, t] - test_target[j, :, t])
+                h_abs = torch.abs(test_target[j, :, t])
+                self.nmse_Gnet_pretest[j, t] = torch.norm(diff_h_abs) ** 2 / torch.norm(
+                    h_abs) ** 2
+                self.out_test_KG[j, t, :, :] = torch.complex(self.Gnet.KG_re,
+                                                             self.Gnet.KG_im)
+
+        end_test = time.time()
+        self.time_test = end_test - start_test
+
+        self.nmse_pretest_epoch[self.ti] = torch.mean(self.nmse_Gnet_pretest, dim=1)
+        self.nmse_pretest_epoch_dB[self.ti] = 10 * torch.log10(
+            self.nmse_pretest_epoch[self.ti])
+        diff_yLoss = self.nmse_pretest_epoch_dB[self.ti] - self.nmse_pretest_epoch_dB[
+            self.ti - 1]
+        print("              nmse_test: {:.2f}[dB]".format(
+            self.nmse_pretest_epoch_dB[self.ti]),
+              "  diff_yLoss: {:.2f}[dB]".format(diff_yLoss),
+              " time: {:.2f}[s]".format(self.time_test))
+
+        torch.cuda.empty_cache()
+
+
+
+
+
+
--- models/Complex_StateSpaceModel.py
+++ models/Complex_StateSpaceModel.py
@@ -1,1 +1,64 @@
+"""
+This file defines the module we used:
+
+StateSpaceModel class: which is for implementing KPIN.
+
+Author: SUN, Yiyong
+Date: 2024.6.30
+"""
+import torch
+import numpy as np
+
+
+class StateSpaceModel:
+
+    def __init__(self, global_vars):
+        self.dev               = global_vars.init_dev
+        Phi                    = global_vars.AR.Phi
+        cov_ut                 = global_vars.AR.cov_ut
+        P                      = global_vars.AR.P
+        cov_vt                 = global_vars.AR.cov_vt
+        kfilter_m2x_init_coeff = global_vars.ssm_m2x_init_coeff
+        tau                    = global_vars.init_tau
+        p                      = global_vars.p
+        M                      = global_vars.init_M
+        N                      = global_vars.init_N
+        m                      = M*N
+        n                      = tau*N
+
+        self.Phi               = torch.from_numpy(Phi).to(torch.complex64).to(self.dev)
+        self.P                 = torch.from_numpy(P).to(torch.complex64).to(self.dev)
+        self.M                 = M
+        self.N                 = N
+        self.p                 = p
+        self.tau               = tau
+        self.rho               = global_vars.init_rho
+        self.m                 = m
+        self.n                 = n
+
+        # Transition Model
+        eye_matrix        = np.eye( (p-1)*m )
+        zeros_matrix      = np.zeros( ((p-1)*m, m) )
+        F                 = np.hstack( [eye_matrix, zeros_matrix] )
+        F                 = np.vstack( [Phi, F] )
+        self.F            = torch.from_numpy(F).to(torch.complex64).to(self.dev)
+        self.F_H          = torch.conj( torch.transpose(self.F, 0, 1) )
+
+        self.cov_ut       = torch.from_numpy(cov_ut).to(torch.complex64).to(self.dev)
+        theta             = np.vstack( [np.eye(m), zeros_matrix] )
+        self.theta        = torch.from_numpy(theta).to(torch.complex64).to(self.dev)
+        self.theta_H      = torch.conj( torch.transpose(self.theta, 0, 1) )
+
+        # Measurement Model
+        zero_vector = np.zeros( (tau*N, (p-1)*m) )
+        G           = np.hstack( [P, zero_vector] )
+        self.G      = torch.from_numpy(G).to(torch.complex64).to(self.dev)
+        self.G_H    = torch.conj( torch.transpose( self.G, 0, 1) )
+
+        self.cov_vt = torch.from_numpy(cov_vt).to(torch.complex64).to(self.dev)
+
+        # Initial latent state
+        self.m1x_0  = torch.squeeze( torch.zeros((p*m, 1), dtype = torch.complex64) ).to(self.dev)  # first-order moment
+        self.m2x_0  = kfilter_m2x_init_coeff * torch.eye( p*m, dtype= torch.complex64).to(self.dev)  # second-order moment
+
 
